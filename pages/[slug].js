0db0e77bda7d2e001fc8c48391204032d14cfb96
Yahya Makarim <ymakarim@gmail.com>
2021-05-26 16:44:51 +0430

    build problem for image of page solved

--- pages/[slug].js
+++ pages/[slug].js
@@ -60,7 +60,7 @@ export async function getStaticProps({ params }) {
   // If the route is like /pages/1, then params.id is 1
   const { slug } = params
 
-  const res = await fetch(`https://reporterly.net/wp-json/wp/v2/pages?slug=${slug}`)
+  const res = await fetch(`https://reporterly.net/wp-json/wp/v2/pages?_embeded=true&slug=${slug}`)
   const page = await res.json()
 
   // Pass page data to the page via props
