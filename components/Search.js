fa7242028b2543845ebb2617c8a8f34875740363
mehdi mohammadi <moh.meh41@gmail.com>
2021-05-27 07:00:19 +0430

    search page and components added

---
+++ components/Search.js
@@ -0,0 +1,9 @@
+export default function Search({query}) {
+
+return (
+    <div>
+        <input type='submit' placeholder='search...' value={query}/>
+    </div>
+);
+
+}
---
+++ pages/search.js
@@ -0,0 +1,13 @@
+export default function Search() {
+    return (
+        <div></div>
+    );
+}
+
+
+export async function getStaticPaths() {
+
+}
+export async function getStaticProps() {
+
+}
