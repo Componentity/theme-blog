a91a9ab9e3436927c6f6da4f556c919f2d39721b
Yahya Makarim <ymakarim@gmail.com>
2021-06-03 09:09:54 +0430

    layout fill objectFit cover

--- components/ImageComponentity.js
+++ components/ImageComponentity.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function ImageComponentity({ src, alt, height = 400, width = 708 }) {
   return (
-    <div className='relative item-detail'>
-      <Image objectFit={'cover'} priority='true' layout='responsive' src={src} alt={alt} />
+    <div className='relative item-detail bg-gray-100'>
+      <Image layout='fill' objectFit='cover' priority='true' src={src} alt={alt} />
     </div>
   )
 }
