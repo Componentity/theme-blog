5a5911c4edd293dd46249d50021c1a1e4b513237
Yahya Makarim <ymakarim@gmail.com>
2021-06-03 08:50:13 +0430

    object fit cover to image component

--- components/ImageComponentity.js
+++ components/ImageComponentity.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function ImageComponentity({ src, alt, height = 400, width = 708 }) {
   return (
     <div className='relative item-detail'>
-      <Image objectFit={'contain'} priority='true' layout='responsive' src={src} alt={alt} />
+      <Image objectFit={'cover'} priority='true' layout='responsive' src={src} alt={alt} />
     </div>
   )
 }
